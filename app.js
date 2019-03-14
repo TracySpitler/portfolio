8c61f980b6823123473e20d0ac452e29a0da1f68
Tracy Spitler <tracyspitler@gmail.com>
2019-03-14 19:24:12 -0400

    created app.js - ejs middleware, public files, and routes, including 404 error

---
+++ app.js
@@ -0,0 +1,24 @@
+// include the express dependency then instantiate it
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// set view engine
+app.set('view engine', 'ejs');
+
+// public folder for js and css files
+app.use(express.static(path.join(__dirname, "public")));
+
+// routes
+const index = require('./routes/index');
+app.use('/', index);
+
+// 404 error handling
+app.use(function(req, res, next) {
+  if (!req.route)
+    res.status(404).render('fourohfour');
+  next();
+});
+
+// export the app
+module.exports = app;
