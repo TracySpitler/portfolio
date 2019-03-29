eaaa50a8e0c255d3719dfd0e2735da3130787ad9
Tracy Spitler <tracyspitler@gmail.com>
2019-03-28 22:35:25 -0400

    updated routes/index.js - created route to email me when someone sends the form

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@
 const express = require('express');
 const router = express.Router();
 var path = require('path');
+var nodemailer = require("nodemailer");
 
 // landing page - GET
 router.get('/', (req, res) => {
@@ -24,6 +25,36 @@ router.get('/about', (req, res) => {
 // contact page - GET
 router.get('/contact', (req, res) => {
   res.render('contact');
+});
+
+// contact me - POST
+router.post('/contact', (req, res) => {
+  var smtpTransport = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.GMAIL_EMAIL,
+      pass: process.env.GMAIL_PASSWORD
+    }
+  });
+
+  var mailOptions = {
+    to: process.env.GMAIL_PRIMARY,
+    from: process.env.GMAIL_EMAIL,
+    subject: 'Portfolio Contact',
+    text: 'Someone is trying to contact you from your portfolio:\n\n' +
+      'Name: ' + req.body.contactName + '\n' +
+      'Email: ' + req.body.contactEmail + '\n' +
+      'Message: ' + req.body.contactMessage
+  };
+
+  smtpTransport.sendMail(mailOptions, function(err) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.render('thankyou');
+    }
+  });
 });
 
 // set up router
